5adb8a2c3913d23ca6cb4ee57050f70a992acc21
Goitseone-Themba <goitseonerozthemba@gmail.com>
2023-10-30 12:39:01 +0200

    changed <Route path='signup' element={ <Signup /> }/> to <Route path='sign' element={ <Signup /> }/>

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,22 @@ import { AnimalTracking } from './pages/AnimalTracking'
 import { CropMonitoring } from './pages/CropMonitoring'
 import { InventoryManagement } from './pages/InventoryManagement'
 import { Login } from './pages/Login'
+import { Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 
 function App() {
   return (
     <div>
       <h1 className='app-label'>Firebase Auth & context</h1>
-         
+         <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/sign' element={<Signup />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/inventoryManagement' element={<InventoryManagement />} />
+            <Route path='/animalTracking' element={<AnimalTracking />} />
+            <Route path='cropMonitoring' element={<CropMonitoring />} />
+         </Routes>
     </div>
   )
 }
