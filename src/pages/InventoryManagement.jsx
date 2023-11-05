5f8f1dd0aeeafc4c46fd6a1e6029c61b2daee87c
Refilwe-Boammaruri <148922645+Refilwe-Boammaruri@users.noreply.github.com>
2023-11-05 18:45:00 +0200

    Update InventoryManagement.jsx

--- src/pages/InventoryManagement.jsx
+++ src/pages/InventoryManagement.jsx
@@ -1,14 +1,10 @@
 import React, { useState } from 'react';
 import { Box, Tab } from '@mui/material';
 import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Button, Stack } from '@mui/material'
+import './CSS/InventoryManagement.css'
 
 export const InventoryManagement = () => {
-  const [value, setValue] = useState('1');
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
   const [value, setValue] = useState('1');
 
   const handleChange = (event, newValue) => {
@@ -18,6 +14,7 @@ export const InventoryManagement = () => {
   return (
     <div>
       <h1>Inventory</h1>
+      <div class='box'>
       <Box>
         <TabContext value={value}>
           <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
@@ -29,6 +26,7 @@ export const InventoryManagement = () => {
               variant="scrollable"
               scrollButtons="auto"
             >
+              
               <Tab label="Equipments" value="1" />
               <Tab label="Fuel" value="2" />
               <Tab label="Fertilisers" value="3" />
@@ -38,7 +36,20 @@ export const InventoryManagement = () => {
               <Tab label="Vaccines" value="7" />
             </TabList>
           </Box>
-          <TabPanel value="1">Content for Equipments</TabPanel>
+          <TabPanel value="1">
+            <div>
+              <table>
+                <tr>
+                  <th>Name of Equipment</th>
+                  <th>Quantity</th>
+                  <th>Condition</th>
+        
+                 <button>Add New</button>
+                 
+                </tr>
+              </table>
+            </div>
+          </TabPanel>
           <TabPanel value="2">Content for Fuel</TabPanel>
           <TabPanel value="3">Content for Fertilisers</TabPanel>
           <TabPanel value="4">Content for Seeds</TabPanel>
@@ -47,37 +58,11 @@ export const InventoryManagement = () => {
           <TabPanel value="7">Content for Vaccines</TabPanel>
         </TabContext>
       </Box>
-      <Box>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList
-              aria-label="Tabs example"
-              onChange={handleChange}
-              textColor="secondary"
-              indicatorColor="secondary"
-              variant="scrollable"
-              scrollButtons="auto"
-            >
-              <Tab label="Equipments" value="1" />
-              <Tab label="Fuel" value="2" />
-              <Tab label="Fertilisers" value="3" />
-              <Tab label="Seeds" value="4" />
-              <Tab label="Animal Feeds" value="5" />
-              <Tab label="Pesticides" value="6" />
-              <Tab label="Vaccines" value="7" />
-            </TabList>
-          </Box>
-          <TabPanel value="1">Content for Equipments</TabPanel>
-          <TabPanel value="2">Content for Fuel</TabPanel>
-          <TabPanel value="3">Content for Fertilisers</TabPanel>
-          <TabPanel value="4">Content for Seeds</TabPanel>
-          <TabPanel value="5">Content for Animal Feeds</TabPanel>
-          <TabPanel value="6">Content for Pesticides</TabPanel>
-          <TabPanel value="7">Content for Vaccines</TabPanel>
-        </TabContext>
-      </Box>
+      </div>
     </div>
   );
 };
+
+
 
 
